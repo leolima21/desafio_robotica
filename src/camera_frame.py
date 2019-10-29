88512647f3641dd546f8d2c8c95324535a577d1f
Leonardo Lima <50239670+leolima21@users.noreply.github.com>
2019-10-28 22:30:50 -0300

    Update camera_frame.py

--- src/camera_frame.py
+++ src/camera_frame.py
@@ -11,47 +11,29 @@ from sensor_msgs.msg import Image
 # Leitura do arquivo haar para criar um classificador
 classificador = cv2.CascadeClassifier('cascades/haarcascade_blackbox_default.xml')
 
-
-
-# Exibicao das imgs capturadas em forma de video
-while True:
-	conectado, frame = video.read()
-	# print(conectado)
-	# print(frame)
-
-		cv2.imshow('Video', frame)
-
-	# Interromper a exibicao
-	if cv2.waitKey(1) == ord('q'):
-		break
-
-# Funcao q ira executar o movimento do robo
 def callback(data):
-    # Criacao do objeto da imagem
-    #ros_frame = data.data
+	# Criacao do objeto de convercao
+	bridge = CvBridge()
 
-    # Criacao do objeto de convercao
-    bridge = CvBridge()
-
-    # Converter a imagem ros para imagem cv2
-    cv2_frame = bridge.imgmsg_to_cv2(data, "bgr8")
-  
-    # Converssao da imagem para escala de cinza
-	  frameCinza = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2GRAY)
+	# Converter a imagem ros para imagem cv2
+	cv2_frame = bridge.imgmsg_to_cv2(data, "bgr8")
+  	
+	# Converssao da imagem para escala de cinza
+	frameCinza = cv2.cvtColor(cv2_frame, cv2.COLOR_BGR2GRAY)
 	
-	  # Deteccao da caixa na imagem 
-	  # minSize = menor obj a ser reconhecido. 30 x 30 eh o padrao
-	  caixasDetectadas = classificador.detectMultiScale(frameCinza, minSize=(50, 50))
+	# Deteccao da caixa na imagem 
+	# minSize = menor obj a ser reconhecido. 30 x 30 eh o padrao
+	caixasDetectadas = classificador.detectMultiScale(frameCinza, minSize=(50, 50))
 	
-	  # Desenho do retangulo para a caixa detectada
-	  for (x, y, largura, altura) in caixasDetectadas:	
-		  # Desenho do retangulo. No final cor e largura da borda
-		  cv2.rectangle(cv2_frame, (x,y), (x + largura, y + largura), (0, 0 , 255), 2)  
-      
-    # Exibir a imagem em uma janela
-    cv2.imshow('frame', cv2_frame)
+	# Desenho do retangulo para a caixa detectada
+	for (x, y, largura, altura) in caixasDetectadas:	
+		# Desenho do retangulo. No final cor e largura da borda
+		cv2.rectangle(cv2_frame, (x,y), (x + largura, y + largura), (0, 0 , 255), 2)  
+      	
+	# Exibir a imagem em uma janela
+	cv2.imshow('frame', cv2_frame)
 
-    cv2.waitKey(1)  
+	cv2.waitKey(1)  
     
     
 def listener():
